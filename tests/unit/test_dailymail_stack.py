8f7e18dd9129caf5a2b6fb63bfbf4d5171208e36
Dranca Cristian <146260539+AlphabeticalAnomaly@users.noreply.github.com>
2023-12-20 16:25:49 +0200

    Added synth tests (#2)

--- tests/unit/test_dailymail_stack.py
+++ tests/unit/test_dailymail_stack.py
@@ -1,15 +1,59 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
-
+import pytest
 from dailymail.dailymail_stack import DailymailStack
 
-# example tests. To run these tests, uncomment this file along with the example
-# resource in dailymail/dailymail_stack.py
-def test_sqs_queue_created():
+
+def test_synthesizes_properly():
     app = core.App()
-    stack = DailymailStack(app, "dailymail")
+    stack = DailymailStack(app, "MyDailyMail")
     template = assertions.Template.from_stack(stack)
 
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+    template.has_resource_properties(
+        "AWS::Lambda::Function",
+        {
+            "Handler": "lambda_function.lambda_handler",
+            "Runtime": "python3.8"
+        },
+    )
+
+    template.has_resource_properties(
+        "AWS::Lambda::Permission",
+        {
+            "Action": "lambda:InvokeFunction",
+            "Principal": "events.amazonaws.com"
+        }
+    )
+
+    template.has_resource_properties(
+        "AWS::Lambda::LayerVersion",
+        {
+            "Content":
+                {
+                    "S3Bucket": {
+                        "Fn::Sub": "cdk-hnb659fds-assets-${AWS::AccountId}-${AWS::Region}"
+                    }
+                }
+        }
+    )
+
+    template.has_parameter(
+        "TestBucket560B80BC",
+        {
+            "type": "AWS::S3::Bucket",
+            "Properties":
+                {
+                  "BucketName": "testbucketcdk1241210"
+                }
+        }
+    )
+
+    template.has_resource_properties(
+        "AWS::Events::Rule",
+        {
+            "ScheduleExpression": "rate(1 day)",
+            "State": "ENABLED"
+        }
+    )
+
+
